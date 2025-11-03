e649bc6243e522e6386d9509691d6755d3f82eaf
jeannemtl <shih.jeanne@gmail.com>
2025-11-03 17:58:36 +0000

    Fix deprecated evaluation_strategy parameter for newer transformers versions

--- model/training/train_uncertainty_classifier.py
+++ model/training/train_uncertainty_classifier.py
@@ -147,7 +147,7 @@ class UncertaintyTrainer:
             weight_decay=0.01,
             logging_dir=f'{self.output_dir}/logs',
             logging_steps=50,
-            evaluation_strategy="epoch",
+            eval_strategy="epoch",
             save_strategy="epoch",
             load_best_model_at_end=True,
             metric_for_best_model="eval_loss",
